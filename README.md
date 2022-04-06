0c480b1c54ee022e75a19849a2a738d5c99fef2b
Luke Sturgill <lukesturgill@gmail.com>
2022-04-05 21:23:22 -0600

    recorded app in use, updated readme

--- README.md
+++ README.md
@@ -1,28 +1,41 @@
 # README Generator
+  
   ## Badge: ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+  
   ## Table of Contents:
-    1. [Description](#description)
-    2. [Installation](#installation)
-    3. [Usage](#usage)
-    4. [License](#license)
-    5. [Contributors](#contributors)
-    6. [Test](#test)
-    7. [Questions](#questions)
+    1. [Description](##description)
+    2. [Installation](##installation)
+    3. [Usage](##usage)
+    4. [License](##license)
+    5. [Contributors](##contributors)
+    6. [Test](##test)
+    7. [Questions](##questions)
+
   ## Description:
   Create a professional Markdown file using CLI.
+  
   ## Installation:
-  node and the inquirer package
-  ## Usage:
-  Answer CLI promts to generate your README file
+  *How to install this application:
+  Node.js and the Inquirer Package
+  
+  ## Usage: 
+  *Programs you'll want to install in order to run this application:
+  Answer the promts as they come up in your command line and a README.md file will be created based upon your responses.
+  
   ## Licenses:
-  1. Your Badge: ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
-  2. Your Badge link: <a href = "https://opensource.org/licenses/MIT">License Link</a>
-  3. Your Badge Readme: <a href = "https://gist.github.com/ckib16/8732561535ed766cd6b8">MIT Guide</a>
+  * License Badge: ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+  * License Link: <a href = "https://opensource.org/licenses/MIT">MIT Link</a>
+  * License Guide: <a href = "https://gist.github.com/ckib16/8732561535ed766cd6b8">MIT Guide</a>
+  
   ## Contributors:
   Luke Sturgill
+  
   ## Test:
+  *How to test this application:
   node index.js
+  
   ## Questions:
-  1. Please click the link for Github: <a href = "https://github.com/lukesturgill">My Github Profile</a>
-  2. Email: lukesturgill@gmaail.com 
+  Please contact me at my email address with any questions!
+  1. My Github: <a href = "https://github.com/lukesturgill">Github</a>
+  2. Email: lukesturgill@gmail.com 
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,57 +2,57 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(data) {
     let licenseBadge = data.license;
-    let yourLicense = '';
+    let selectedBadge = '';
     if(licenseBadge === 'MIT') {
-      yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+      selectedBadge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
     } else if (licenseBadge === 'Mozilla Public License 2.0') {
-      yourLicense = `![License: Mozilla Public License 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`
+      selectedBadge = `![License: Mozilla Public License 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`
     } else if (licenseBadge === 'Apache License 2.0') {
-      yourLicense = `![License: Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+      selectedBadge = `![License: Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
     } else if (licenseBadge === 'Boost Software License 1.0') {
-    yourLicense = `![License: Boost Software License 1.0](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`
+    selectedBadge = `![License: Boost Software License 1.0](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`
     }else {
-      yourLicense = '';
+      selectedBadge = '';
     }
-    return yourLicense;
+    return selectedBadge;
   };
 
 // Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(data) {
     let licenseLink = data.license;
-    let yourLink = '';
+    let selectedLink = '';
     if(licenseLink === 'MIT') {
-      yourLink = `<a href = "https://opensource.org/licenses/MIT">MIT Link</a>`
+      selectedLink = `<a href = "https://opensource.org/licenses/MIT">MIT Link</a>`
     } else if (licenseLink === 'Mozilla Public License 2.0') {
-      yourLink = `<a href = "https://opensource.org/licenses/MPL-2.0">Mozilla Link</a>`
+      selectedLink = `<a href = "https://opensource.org/licenses/MPL-2.0">Mozilla Link</a>`
     } else if (licenseLink === 'Apache License 2.0') {
-      yourLink = `<a href = "https://opensource.org/licenses/Apache-2.0">Apache Link</a>`
+      selectedLink = `<a href = "https://opensource.org/licenses/Apache-2.0">Apache Link</a>`
     } else if(licenseLink === 'Boost Software License 1.0') {
-      yourLink = `<a href = "https://www.boost.org/LICENSE_1_0.txt">Boost Link</a>`
+      selectedLink = `<a href = "https://www.boost.org/LICENSE_1_0.txt">Boost Link</a>`
     } else {
-      yourLink = '';
+      selectedLink = '';
     }
-  return yourLink;
+  return selectedLink;
 }
 
 // Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(data) {
     let licenseSection = data.license;
-    let yourSection = ''
+    let selectedSection = ''
     if(licenseSection === 'MIT') {
-      yourSection = `<a href = "https://gist.github.com/ckib16/8732561535ed766cd6b8">MIT Guide</a>`
+      selectedSection = `<a href = "https://gist.github.com/ckib16/8732561535ed766cd6b8">MIT Guide</a>`
     } else if(licenseSection === 'Mozilla Public License 2.0') {
-      yourSection = `<a href = "https://www.mozilla.org/en-US/MPL/2.0/">Mozilla Guide</a>`
+      selectedSection = `<a href = "https://www.mozilla.org/en-US/MPL/2.0/">Mozilla Guide</a>`
     } else if(licenseSection === 'Apache License 2.0') {
-      yourSection = `<a href = "https://github.com/Medium/opensource/blob/master/apache-license-2.0.md">Apache Guide</a>`
+      selectedSection = `<a href = "https://github.com/Medium/opensource/blob/master/apache-license-2.0.md">Apache Guide</a>`
     }else if(licenseSection === 'Boost Software License 1.0') {
-      yourSection = `<a href = "https://www.boost.org/users/license.html">Boost Guide</a>`
+      selectedSection = `<a href = "https://www.boost.org/users/license.html">Boost Guide</a>`
     } else {
-      yourSection = '';
+      selectedSection = '';
     }
-    return yourSection;
+    return selectedSection;
   }
 
 // Function to generate markdown for README
@@ -84,7 +84,7 @@ function generateMarkdown(data) {
   ## Licenses:
   * License Badge: ${renderLicenseBadge(data)}
   * License Link: ${renderLicenseLink(data)}
-  * License Page: ${renderLicenseSection(data)}
+  * License Guide: ${renderLicenseSection(data)}
   
   ## Contributors:
   ${data.contributors}
@@ -95,7 +95,7 @@ function generateMarkdown(data) {
   
   ## Questions:
   Please contact me at my email address with any questions!
-  1. My Github: <a href = "https://github.com/${data.username}">" "</a>
+  1. My Github: <a href = "https://github.com/${data.username}">Github</a>
   2. Email: ${data.email} 
   `;
   }
